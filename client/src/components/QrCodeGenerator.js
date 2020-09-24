48484de34a5106a08a96b18dc7e272e9feb3ba14
Your Name <dhdezllerena94@gmail.com>
2020-09-24 13:36:54 +0200

    generate pdf of the qrCode

--- client/src/components/QrCodeGenerator.js
+++ client/src/components/QrCodeGenerator.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useParams, NavLink } from 'react-router-dom';
 import QRCode from 'qrcode.react'
+import ReactToPdf from 'react-to-pdf'
 
+const ref = React.createRef();
+console.log( '----------> ref:', ref);
 
 function QrCodeGenerator () {
 
   const {userName} = useParams();
   const URLpath = `http://localhost:3000/homepage/${userName}/mymenu`
   console.log( '----------> URLpath:', URLpath);
-
-
 
   return (
     <div>
@@ -19,9 +20,18 @@ function QrCodeGenerator () {
           Back
         </NavLink>
         </div>
-        <div className="qrcode">
-          <QRCode value={URLpath} alt={URLpath}/>
-        </div>
+
+        <ReactToPdf targetRef={ref} filename={userName}>
+        {({toPdf}) => (
+            <button onClick={toPdf}>Generate pdf</button>
+        )}
+        </ReactToPdf>
+
+      <div ref={ref} id="QR-Code" className="qrcode">
+        <h1>hola</h1>
+        <QRCode  value={URLpath} alt={URLpath}/>
+      </div>
+
     </div>
   )
 }
